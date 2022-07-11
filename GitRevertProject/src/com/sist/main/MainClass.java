716abfba614a760056b5812b620ff391bdbe75d5
chaijewon <vcandjava@nate.com>
2022-07-11 18:27:47 +0900

    commit_10

--- GitRevertProject/src/com/sist/main/MainClass.java
+++ GitRevertProject/src/com/sist/main/MainClass.java
@@ -1,11 +1,23 @@
 package com.sist.main;
-
+/*
+ *     git log --oneline -n 3
+ *     -- (HEAD)
+ *     --
+ *     --
+ *     
+ *     git format-patch -1 (HEAD=> ID)
+ *     
+ *     git revert HEAD
+ *     
+ *     ==> rollback 
+ */
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello Revert Commit -1");
         System.out.println("Hello Revert Commit -2");
+        System.out.println("Hello Revert Commit -3");
 	}
 
 }
