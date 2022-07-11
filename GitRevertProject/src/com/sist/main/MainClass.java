dc16373177e5772c1eb0b048afc07b2c608b1018
chaijewon <vcandjava@nate.com>
2022-07-11 18:16:57 +0900

    commit-1

---
+++ GitRevertProject/src/com/sist/main/MainClass.java
@@ -0,0 +1,11 @@
+package com.sist.main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello Revert Commit -1");
+        System.out.println("Hello Revert Commit -2");
+	}
+
+}
