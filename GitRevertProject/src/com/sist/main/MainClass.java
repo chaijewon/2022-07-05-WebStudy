ecb267470d2f04a6236526c7d6dbff41a99ee729
chaijewon <vcandjava@nate.com>
2022-07-11 18:23:03 +0900

    Revert "commit-2"
    This reverts commit c44f7d37ea41873bdefa66028f70bf001ed99da3.

--- GitRevertProject/src/com/sist/main/MainClass.java
+++ GitRevertProject/src/com/sist/main/MainClass.java
@@ -6,7 +6,6 @@ public class MainClass {
 		// TODO Auto-generated method stub
         System.out.println("Hello Revert Commit -1");
         System.out.println("Hello Revert Commit -2");
-        System.out.println("Hello Revert Commit -3");
 	}
 
 }
