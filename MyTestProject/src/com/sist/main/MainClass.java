c464bcb8cf5023d78e482ce747a06a7768210e1d
chaijewon <vcandjava@nate.com>
2022-07-11 17:40:00 +0900

    commit_1

---
+++ MyTestProject/src/com/sist/main/MainClass.java
@@ -0,0 +1,11 @@
+package com.sist.main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello ~~ -1");
+        System.out.println("Hello ~~ -2");
+	}
+
+}
