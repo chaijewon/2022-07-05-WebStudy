9bbc73bcaa1759c9c07c6141e51a71835255a59e
chaijewon <vcandjava@nate.com>
2022-07-15 14:52:13 +0900

    1111

--- JSPBasicProject1/src/main/webapp/music/music.jsp
+++ JSPBasicProject1/src/main/webapp/music/music.jsp
@@ -1,40 +1,47 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" import="java.util.*,com.sist.dao.*"%>
+<%
+    MusicDAO dao=new MusicDAO();
+    List<MusicVO> list=dao.musicListData(1);
+%>
 <!DOCTYPE html>
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CodePen - Filter Movies using sort()</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <title>지니뮤직</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'><link rel="stylesheet" href="./style.css">
 
 </head>
 <body>
 <!-- partial:index.partial.html -->
 <header>  
-  <select id="rating-filter">
-    <option value="">Sort</option>
-    <option value="Low rating">Low rating</option>
-    <option value="High rating">High rating</option>
-    <option value="A-Z">A-Z</option>
-    <option value="Z-A">Z-A</option>
-  </select>
+  
 </header>
 
 <section></section>
 
 <template id="movie-template">
+ <%
+   for(MusicVO vo:list)
+   {
+ %>
   <figure>
-    <img src="{{poster}}" />
+    <img src="<%= vo.getPoster() %>" />
     <figcaption>
-      <p>{{genre}}</p>
+      <p><%=vo.getTitle() %></p>
       <div class="rating">
         <i class="fa fa-heart"></i>
-        <h4>{{rating}}</h4>
+        <h4><%=vo.getSinger() %></h4>
       </div>
     </figcaption>
   </figure>
+  <%
+   }
+  %>
 </template>
 <!-- partial -->
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min.js'></script><script  src="./script.js"></script>
 
 </body>
--- JSPBasicProject1/src/main/webapp/music/script.js
+++ JSPBasicProject1/src/main/webapp/music/script.js
@@ -1,122 +1,119 @@
-function init() {
-  // listType: top_rated, popular, now_playing, latest, upcoming
-  function requestHandler({
-    method = "GET",
-    pageNumber = 1,
-    listType = "popular" })
+const movies = [
   {
-    return new Promise((resolve, reject) => {
-      var request = new XMLHttpRequest();
-      request.open(
-      method,
-      `https://api.themoviedb.org/3/movie/${listType}?api_key=4bef8838c2fd078bd13d7127d8dedcd4&language=en-US&page=${pageNumber}`);
-
-      request.setRequestHeader(
-      "Content-type",
-      "application/json; charset=utf-8");
-
-      request.addEventListener("load", handleDataLoad, false);
-      request.addEventListener("error", handleError, false);
-      request.send();
-
-      function handleError(event) {
-        reject("The resquest has been rejected");
-      }
-
-      function handleDataLoad(event) {
-        resolve({
-          listType,
-          res: JSON.parse(event.target.response) });
-
-      }
-    });
+    title: "Mad Max: Fury Road",
+    genre: ["Action", "Adventure", "Sci-Fi"],
+    rating: 8.3,
+    poster:
+      "https://s-media-cache-ak0.pinimg.com/originals/ce/0c/93/ce0c93d50ae68922d1f4f6667c95e1a8.jpg"
+  },
+  {
+    title: "The Hunger Games: Mockingjay Part 1",
+    genre: ["Adventure", "Sci-Fi", "Thriller"],
+    rating: 6.8,
+    poster:
+      "https://1.bp.blogspot.com/-Ds0sudZmSq4/Vgxrr75E77I/AAAAAAAAREo/sZkZW5YMDTw/s1600/Mockingjay%2BPart%2B1.jpg"
+  },
+  {
+    title: "Jurassic World",
+    genre: ["Action", "Adventure", "Sci-Fi"],
+    rating: 7.2,
+    poster:
+      "https://s-media-cache-ak0.pinimg.com/736x/0b/ab/9a/0bab9a9c671dbb7aa8626eec44a0195f.jpg"
+  },
+  {
+    title: "Everest",
+    genre: ["Adventure", "Drame", "Thriller"],
+    rating: 7.4,
+    poster: "http://www.impawards.com/2015/posters/everest_ver4.jpg"
+  },
+  {
+    title: "Insurgent",
+    genre: ["Adventure", "Sci-Fi", "Thriller"],
+    rating: 6.4,
+    poster:
+      "http://cdn2-www.comingsoon.net/assets/uploads/2015/01/FIN16_Insurgent_Guns_1Sht_Trim-1422379653-mtv-14224534611.jpg"
+  },
+  {
+    title: "Sicario",
+    genre: ["Action", "Crime", "Drama"],
+    rating: 7.6,
+    poster:
+      "https://s-media-cache-ak0.pinimg.com/564x/7f/a1/5c/7fa15c26aa2cb48562ea37bbc177be74.jpg"
+  },
+  {
+    title: "Green Lantern",
+    genre: ["Action", "Adventure", "Sci-Fi"],
+    rating: 5.6,
+    poster:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMyMTg3OTM5Ml5BMl5BanBnXkFtZTcwNzczMjEyNQ@@._V1_UX182_CR0,0,182,268_AL_.jpg"
+  },
+  {
+    title: "Prisoners",
+    genre: ["Crime", "Drama", "Mystery"],
+    rating: 8.1,
+    poster:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg0NTIzMjQ1NV5BMl5BanBnXkFtZTcwNDc3MzM5OQ@@._V1_UX182_CR0,0,182,268_AL_.jpg"
+  },
+  {
+    title: "The Hateful Eight",
+    genre: ["Crime", "Drama", "Mystery"],
+    rating: 7.8,
+    poster:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA1MTc1NTg5NV5BMl5BanBnXkFtZTgwOTM2MDEzNzE@._V1_UX182_CR0,0,182,268_AL_.jpg"
   }
-  function crtEle(elementName, text, attribute = {}) {
-    const newEle = document.createElement(elementName);
-    Object.keys(attribute).forEach(key => {
-      newEle.setAttribute(key, attribute[key]);
-    });
+];
 
-    if (!!text) {
-      newEle.innerHTML = text;
-    }
+const rating_filter = $("#rating-filter");
+const template = $("#movie-template").html();
+const section = $("section");
+let list = "";
 
-    return newEle;
+rating_filter.on("change", sort);
+
+// Loop through movies and display them
+movies.forEach(movie => (list += Mustache.render(template, movie)));
+section.html(list);
+
+function sort() {
+  let value = this.value;
+
+  if (!value) {
+    // Display all movies when there is no value
+    section.html(list);
+  } else if (value == "Low rating") {
+    let lowRating = "";
+
+    // Sort movies by low rating
+    const sortLow = movies
+      .sort((a, b) => (a.rating > b.rating ? 1 : -1))
+      .map(movie => (lowRating += Mustache.render(template, movie)));
+    section.html(lowRating);
+  } else if (value == "High rating") {
+    let highRating = "";
+
+    // Sort movies by high rating
+    const sortHigh = movies
+      .sort((a, b) => (b.rating > a.rating ? 1 : -1))
+      .map(movie => (highRating += Mustache.render(template, movie)));
+    section.html(highRating);
+  } else if (value == "A-Z") {
+    let ascending = "";
+
+    // Sort movies by ascending order
+    const a_z = movies
+      .sort((a, b) => (strip(a.title) > strip(b.title) ? 1 : -1))
+      .map(m => (ascending += Mustache.render(template, m)));
+    section.html(ascending);
+  } else if (value == "Z-A") {
+    let descending = "";
+    // Sort movies in descending order
+    const z_a = movies
+      .sort((a, b) => (strip(b.title) > strip(a.title) ? 1 : -1))
+      .map(m => (descending += Mustache.render(template, m)));
+    section.html(descending);
   }
-  function getFormatedDate(date, format) {
-    return moment(date).format(format);
-  }
-  const savedLikedList = [];
-  const headerCopy = {
-    popular: "The most popular movies" };
-
-  const switchTabs = document.querySelector("#switch-tab");
-  const movieList = document.querySelector("#movie-list");
-  const likedList = document.querySelector("#liked-list");
-  const listHeading = document.querySelector("#movies-list__heading");
-  const BASE_IMG_URL = "https://image.tmdb.org/t/p/w500/";
-
-  requestHandler({ method: "GET" }).then(data => {
-    listHeading.innerHTML = headerCopy[data.listType];
-
-    data.res.results.forEach((movie, index) => {
-      const movieCard = crtEle("div", null, {
-        class: "movie_card",
-        id: movie.id,
-        "data-id": movie.id,
-        "data-index": index,
-        "data-movie-card": "",
-        'data-title': movie.title });
-
-      const img = crtEle("img", null, {
-        alt: movie.title,
-        src: BASE_IMG_URL + movie.poster_path,
-        class: "movie_card_poster",
-        width: 200,
-        height: 300 });
-
-      const title = crtEle("h3", movie.title, {
-        class: "movie_card_title" });
-
-      const formattedDate = getFormatedDate(movie.release_date, "MMM D, YYYY");
-      const releaseDate = crtEle("div", formattedDate, {
-        class: "movie_card_release-date" });
-
-      const actionsSection = crtEle('section', null, {
-        class: 'movie_card_actions_sec' });
-
-      const like = crtEle('a', 'Like It!', {
-        class: 'movie_card_action_like',
-        href: 'javascript:void(0)' });
-
-      const dislike = crtEle('a', 'Hate It!', {
-        class: 'movie_card_action_dislike',
-        href: 'javascript:void(0)' });
-
-
-      if (!!savedLikedList.find(({ id }) => id === movie.id)) {
-        like.classList.add('liked');
-      }
-
-      actionsSection.appendChild(like);
-
-      movieCard.appendChild(img);
-      movieCard.appendChild(title);
-      movieCard.appendChild(releaseDate);
-      movieCard.appendChild(actionsSection);
-      movieList.appendChild(movieCard);
-    });
-  });
-
-  document.querySelectorAll("div.tab").forEach(function (tab) {
-    tab.addEventListener("click", function (event) {
-      Array.from(switchTabs.children).forEach(child => {
-        child.classList.remove("selected");
-      });
-      event.target.classList.add("selected");
-    });
-  });
-
 }
 
-document.addEventListener("DOMContentLoaded", init);
+function strip(title) {
+  return title.replace(/^(a|the|an)\s/i, "");
+}
